1a8e632d88338c3b49634546829ab8d6669a23b0
ytlee <dobidugi@gmail.com>
2024-10-06 22:03:37 +0900

    test: UserService Crfeate, Update 테스트 코드 추가

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,12 +1,19 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import com.example.demo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -20,6 +27,9 @@ class UserServiceTest {
 
   @Autowired
   private UserRepository userRepository;
+
+  @MockBean
+  private JavaMailSender mailSender;
 
   @Test
   void getByEmail은_Active_상태의_유저를_가져온다() {
@@ -99,5 +109,117 @@ class UserServiceTest {
       userService.getById(userEntity.getId());
     }).isInstanceOf(ResourceNotFoundException.class);
 
+  }
+
+  @Test
+  void create는_유저를_생성할_수_있다() {
+    // given
+    UserCreateDto dto = UserCreateDto.builder()
+            .email("dobidugi@gmail.com")
+            .address("서울시 강남구")
+            .nickname("dobidugi")
+            .build();
+
+    //when
+    UserEntity createUser = userService.create(dto);
+    BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
+
+
+    //then
+    assertThat(createUser.getId()).isNotNull();
+    assertThat(createUser.getStatus()).isEqualTo(UserStatus.PENDING);
+  }
+
+  @Test
+  void update는_유저를_수정할_수_있다() {
+    // given
+    UserCreateDto dto = UserCreateDto.builder()
+            .email("dobidugi@gmail.com")
+            .address("서울시 강남구")
+            .nickname("dobidugi")
+            .build();
+
+    UserEntity createUser = userService.create(dto);
+    BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
+    createUser.setStatus(UserStatus.ACTIVE);
+    userRepository.flush();
+
+    UserUpdateDto updateDto = UserUpdateDto.builder()
+            .nickname("dobidugi2")
+            .address("인천시")
+            .build();
+
+
+    //when
+    UserEntity updateUser = userService.update(createUser.getId(), updateDto);
+
+
+    //then
+    UserEntity findUser = userRepository.getById(createUser.getId());
+    assertThat(findUser.getId()).isNotNull();
+    assertThat(findUser.getNickname()).isEqualTo(updateDto.getNickname());
+    assertThat(findUser.getAddress()).isEqualTo(updateDto.getAddress());
+  }
+
+  @Test
+  void user를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+    //given
+    UserCreateDto dto = UserCreateDto.builder()
+            .email("dobidugi@gmail.com")
+            .address("서울시 강남구")
+            .nickname("dobidugi")
+            .build();
+    UserEntity createUser = userService.create(dto);
+    BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
+    createUser.setStatus(UserStatus.ACTIVE);
+
+    //when
+    userService.login(createUser.getId());
+
+    //then
+    UserEntity findUser = userRepository.getById(createUser.getId());
+    assertThat(findUser.getLastLoginAt()).isNotNull();
+    assertThat(findUser.getLastLoginAt()).isGreaterThan(0L);
+  }
+
+  @Test
+  void PENDING_상태의_사용자는_인증코드로_ACTIVE_로_변경할_수_있다() {
+    //given
+    UserCreateDto dto = UserCreateDto.builder()
+            .email("dobidugi@gmail.com")
+            .address("서울시 강남구")
+            .nickname("dobidugi")
+            .build();
+    UserEntity createUser = userService.create(dto);
+    BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
+    createUser.setStatus(UserStatus.ACTIVE);
+    createUser.setCertificationCode("aaaaaa-aaaaa-aaaaa-aaaaa-aaaaa");
+
+    //when
+    userService.verifyEmail(createUser.getId(), "aaaaaa-aaaaa-aaaaa-aaaaa-aaaaa");
+
+    //then
+    UserEntity findUser = userRepository.getById(createUser.getId());
+    assertThat(findUser.getStatus()).isEqualTo(UserStatus.ACTIVE);
+  }
+
+  @Test
+  void PENDING_상태의_사용자는_인증코드_실패시_에러를_발생_시킨다() {
+    //given
+    UserCreateDto dto = UserCreateDto.builder()
+            .email("dobidugi@gmail.com")
+            .address("서울시 강남구")
+            .nickname("dobidugi")
+            .build();
+    UserEntity createUser = userService.create(dto);
+    BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
+    createUser.setStatus(UserStatus.ACTIVE);
+    createUser.setCertificationCode("aaaaaa-aaaaa-aaaaa-aaaaa-aaaaa");
+
+    //when
+    //then
+    assertThatThrownBy(() -> {
+      userService.verifyEmail(createUser.getId(), "bbbb-bbbbb-bbbbbb");
+    }).isInstanceOf(CertificationCodeNotMatchedException.class);
   }
 }
